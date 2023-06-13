caddfeda6d4506491e4cfc81f44f685a0e7c33a0
crypticx5 <tyadgirkar@gmail.com>
2023-06-12 18:59:08 -0700

    k

--- src/subsystems/drive.cpp
+++ src/subsystems/drive.cpp
@@ -4,8 +4,9 @@ MotorGroup leftDrive({-1,-2,-3});
 MotorGroup rightDrive({4,5,6});
 
 Controller controller;
-updateDrive(){
+void updateDrive(){
     double leftPower = controller.getAnalog(ControllerAnalog::leftY);
     double rightPower = controller.getAnalog(ControllerAnalog::rightY);
-    leftDrive.
+    leftDrive.moveVoltage(leftPower);
+    rightDrive.moveVoltage(rightPower);
 }
