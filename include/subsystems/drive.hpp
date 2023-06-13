983212627633eddbeb94d6066f68d78bd111d567
crypticx5 <tyadgirkar@gmail.com>
2023-06-12 18:55:31 -0700

    l

--- include/subsystems/drive.hpp
+++ include/subsystems/drive.hpp
@@ -1,6 +1,14 @@
 #include "main.h"
 #include "okapi/impl/device/motor/motorGroup.hpp"
 
+extern Motor leftFront;
+extern Motor leftMid;
+extern Motor leftBack;
+extern Motor rightFront;
+extern Motor rightMid;
+extern Motor rightBack;
+
 extern MotorGroup leftDrive;
 extern MotorGroup rightDrive;
-
+void updateDrive();
+extern Controller controller;
--- src/subsystems/drive.cpp
+++ src/subsystems/drive.cpp
@@ -1,1 +1,11 @@
 #include "main.h"
+
+MotorGroup leftDrive({-1,-2,-3});
+MotorGroup rightDrive({4,5,6});
+
+Controller controller;
+updateDrive(){
+    double leftPower = controller.getAnalog(ControllerAnalog::leftY);
+    double rightPower = controller.getAnalog(ControllerAnalog::rightY);
+    leftDrive.
+}
