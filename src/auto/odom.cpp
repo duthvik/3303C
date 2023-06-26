fa64b5d9a7e2ee122e8214a0889a5d7988f90673
duthvik <ruthvikd2@gmail.com>
2023-06-26 10:11:40 -0700

    c

--- src/auto/odom.cpp
+++ src/auto/odom.cpp
@@ -1,1 +1,21 @@
 #include "main.h"
+
+void Odom::setPosition(double X_position, double Y_position, double orientation_deg, double ForwardTracker_position, double SidewaysTracker_position)
+{
+    Xpos = X_position;
+    Ypos = Y_position;
+    this->orientation_deg = orientation_deg;
+    FTrackerPos = ForwardTracker_position;
+    STrackerPods = SidewaysTracker_position;
+}
+
+void Odom::set_physical_distances(double ForwardTracker_center_distance, double SidewaysTracker_center_distance)
+{
+    FTrackerCenterDist = ForwardTracker_center_distance;
+    STrackerCenterDist = SidewaysTracker_center_distance;
+}
+
+void Odom::updatePosition(double ForwardTracker_position, double SidewaysTracker_position, double orientation_deg)
+{
+    
+}
