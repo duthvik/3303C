858bcae2e4051dc0a96feff8ecd135f0bd47bc37
duthvik <ruthvikd2@gmail.com>
2023-07-14 16:50:08 -0700

    drive code

--- src/subsystems/drive.cpp
+++ src/subsystems/drive.cpp
@@ -1,11 +1,11 @@
 #include "main.h"
 
-Motor rightTop(18, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor rightMid(1, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor rightBack(7, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor leftTop(8, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor leftMid(5, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
-Motor leftBack(2, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor rightTop(6, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor rightMid(5, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor rightBack(4, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor leftTop(9, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor leftMid(8, true, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
+Motor leftBack(7, false, AbstractMotor::gearset::blue, AbstractMotor::encoderUnits::degrees);
 
 std::shared_ptr<OdomChassisController> drive =
      ChassisControllerBuilder()
